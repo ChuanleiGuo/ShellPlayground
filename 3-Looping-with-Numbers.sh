04e91b9010ee6bc955bb2185ec869798615c8c5a
ChuanleiGuo <chuanleiguo@gmail.com>
2018-07-25 09:56:31 +0800

    loop with numbers

---
+++ 3-Looping-with-Numbers.sh
@@ -0,0 +1,26 @@
+# Use for loops to display the natural numbers from 1 to 50.
+# Input Format
+# There is no input
+# Constraints
+# Output Format
+# 1
+# 2
+# 3
+# 4
+# 5
+# .
+# .
+# .
+# .
+# .
+# 50
+# Sample Input
+# Sample Output
+# -
+# Explanation
+# -
+
+for number in {1..50}
+do
+    echo $number
+done
