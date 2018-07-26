697af3f19e9f3a262d85b2e71454e4c51a0cb72b
guochuanlei <guochuanlei@baidu.com>
2018-07-26 10:09:48 +0800

    5

---
+++ 5-The_World_of_Numbers.sh
@@ -0,0 +1,31 @@
+# Given two integers, x and y , find their sum, difference, product, and quotient.
+# Input Format
+# Two lines containing one integer each ( and , respectively).
+# Constraints
+
+# -100 <= x, y <= 100
+# y != 0
+
+# Output Format
+# Four lines containing the sum (), difference (), product (), and quotient (), respectively. 
+# (While computing the quotient, print only the integer part.)
+# Sample Input
+# 5
+# 2
+# Sample Output
+# 7
+# 3
+# 10
+# 2
+# Explanation
+# 5 + 2 = 7 
+# 5 - 2 = 3 
+# 5 * 2 = 10 
+# 5 / 2 = 2 (Integer part)
+
+read num1 num2
+
+echo $((num1 + num2))
+echo $((num1 - num2))
+echo $((num1 * num2))
+echo $((num1 / num2))
