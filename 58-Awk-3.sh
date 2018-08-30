b0aacb76a39371d7a121fc4ecfa5bedbba9093a4
guochuanlei <guochuanlei@baidu.com>
2018-08-30 19:10:17 +0800

    awk and sed

---
+++ 58-Awk-3.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+awk '{
+    avg=($2+$3+$4)/3;
+    if (avg >= 50 && avg< 60)
+        print $1,$2,$3,$4, ": C"
+    else if (avg >= 60 && avg < 80)
+        print $1,$2,$3,$4, ": B"
+    else if (avg >= 80)
+        print $1,$2,$3,$4, ": A" 
+    else 
+        print $1,$2,$3,$4, ": FAIL"
+}'
